0df6622a58f94c653175dfdf6538f8849c0acca9
linyuelin <linyuelin1997@gmail.com>
2024-03-13 00:01:03 +0900

    no message

--- MyTableviewPlaceholder/MyTableviewPlaceholder/ViewController.swift
+++ MyTableviewPlaceholder/MyTableviewPlaceholder/ViewController.swift
@@ -6,14 +6,48 @@
 //
 
 import UIKit
+import HGPlaceholders
 
-class ViewController: UIViewController {
+class ViewController: UIViewController , UITableViewDelegate , UITableViewDataSource {
+    
+    private let tableView = TableView()
+    
+    var data = [String]()
+    
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        tableView.register(UITableViewCell.self, forCellReuseIdentifier: "cell")
+        view.addSubview(tableView)
+        tableView.delegate = self
+        tableView.dataSource = self
+        tableView.frame = view.bounds
+        tableView.backgroundColor = .link
+        tableView.showLoadingPlaceholder()
+        
+        DispatchQueue.main.asyncAfter(deadline: .now() + 3 , execute: {
+            
+            self.tableView.showErrorPlaceholder()
+           
+        })
     }
+        
+//        if data.isEmpty {
+//            tableView.isHidden = true
+//            let imageView = UIImageView(frame: .zero)
+//        }
+    
 
-
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return data.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        cell.textLabel?.text = data[indexPath.row]
+        return cell
+    }
 }
 
